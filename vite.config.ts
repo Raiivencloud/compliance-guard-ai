0d9d9c0020012dec43d535188d99fc4b2f472c5e
Raiivencloud <agusgestro17@gmail.com>
2026-04-20 16:42:50 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@google/generative-ai']
-  }
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
 });
