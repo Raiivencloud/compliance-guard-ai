7af62b541116561ffa6d960be9cd1a502f150bad
Raiivencloud <agusgestro17@gmail.com>
2026-04-21 13:08:10 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,22 +4,22 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD4DFwYz_i_pHq01JKXeW__J_Bj4p7fo8M",
-  authDomain: "complianceguardai.firebaseapp.com",
-  projectId: "complianceguardai",
-  storageBucket: "complianceguardai.firebasestorage.app",
+  // USAMOS EL ID EXACTO QUE APARECE EN TU CONSOLA (Imagen bd0cc1)
+  authDomain: "compliance-guard-ai.firebaseapp.com",
+  projectId: "compliance-guard-ai",
+  storageBucket: "compliance-guard-ai.appspot.com",
   messagingSenderId: "12567186833",
   appId: "1:12567186833:web:468a381acf94ac34a5e30"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exportar servicios
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+
 export const db = getFirestore(app);
 
-// ESTO PERMITE QUE EL BOTÓN SECRETO FUNCIONE
 (window as any).db = db;
 
 export default app;
