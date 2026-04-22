ab98273f06efc380b72fe1ad4da358f68db42fac
Raiivencloud <agusgestro17@gmail.com>
2026-04-22 09:57:38 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion ... } from 'framer-motion';
 import { Shield, Lock, Globe, CheckCircle2, MessageCircle, Info } from 'lucide-react';
 import Header from './components/common/Header';
 import Hero from './components/landing/Hero';
