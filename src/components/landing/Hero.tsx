44fb1e737f6dd1a52b0b799792939fc81cae522b
Raiivencloud <agusgestro17@gmail.com>
2026-04-22 10:05:04 -0300

    Update Hero.tsx

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,4 +1,4 @@
-import { motion ... } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useTranslation } from '../../hooks/useTranslation';
 
 export default function Hero() {
